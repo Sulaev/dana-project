5adb299b7ed25f0e5e1009735a34d7c7102acb4a
Sulaev <sulaev2004@mail.ru>
2025-04-27 14:46:12 +0300

    add logoText.svg

--- src/component/header.tsx
+++ src/component/header.tsx
@@ -1,16 +1,17 @@
 import { ROUTES } from "@/config/routes";
 import Logo from "@/assets/logo.svg";
+import LogoText from "@/assets/logoText.svg";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="flex justify-between w-full h-20">
-      <div className="flex gap-3 items-center p-7">
-        <Link href={ROUTES.home}>
+      <Link href={ROUTES.home}>
+        <div className="flex gap-3 items-center p-7">
           <Logo />
-        </Link>
-        <div>Dana Bakhtina</div>
-      </div>
+          <LogoText />
+        </div>
+      </Link>
       <div className="flex justify-between items-center gap-2">
         <div className="px-2.5">Обо мне</div>
         <div className="px-2.5">Проекты</div>
