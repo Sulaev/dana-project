c7de80abe681603d9f29e68de556e625aec80f6e
Sulaev <sulaev2004@mail.ru>
2025-04-28 18:28:02 +0300

    remake headers navigations tag

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,29 @@ import Logo from "@/assets/logo.svg";
 import LogoText from "@/assets/logoText.svg";
 import Link from "next/link";
 
+const Navbar = [
+  {
+    tittle: "Обо мне",
+    link: "/",
+  },
+  {
+    tittle: "Проекты",
+    link: "/",
+  },
+  {
+    tittle: "Услуги",
+    link: "/",
+  },
+  {
+    tittle: "Блог",
+    link: "/",
+  },
+  {
+    tittle: "Контакты",
+    link: "/",
+  },
+];
+
 export default function Header() {
   return (
     <header className="flex justify-between w-full h-20">
@@ -14,11 +37,13 @@ export default function Header() {
       </Link>
       <div className="flex items-center gap-12">
         <div className="flex justify-between items-center gap-2">
-          <div className="px-2.5">Обо мне</div>
-          <div className="px-2.5">Проекты</div>
-          <div className="px-2.5">Услуги</div>
-          <div className="px-2.5">Блог</div>
-          <div className="px-2.5">Контакты</div>
+          {Navbar.map((tag) => {
+            return (
+              <Link href={tag.link} key={tag.tittle}>
+                <div className="px-2.5">{tag.tittle}</div>
+              </Link>
+            );
+          })}
         </div>
         <div className="p-7">EN</div>
       </div>
