882fc143beea5831eb5825000fd8a995d25a9d2d
Sulaev <sulaev2004@mail.ru>
2025-04-28 18:53:51 +0300

    remake footer tag list

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,41 +5,58 @@ import TelegramIcon from "@/assets/telegram-Icon.svg";
 import MailIcon from "@/assets/mail-Icon.svg";
 import SubstractIcon from "@/assets/subtract-Icon.svg";
 
+const NavigationLink = [
+  {
+    tittle: "Обо мне",
+    link: "/",
+  },
+  {
+    tittle: "Проекты",
+    link: "/",
+  },
+  {
+    tittle: "Услуги",
+    link: "/",
+  },
+  {
+    tittle: "Блог",
+    link: "/",
+  },
+  {
+    tittle: "Контакты",
+    link: "/",
+  },
+  {
+    tittle: "Фирменный стиль",
+    link: "/",
+  },
+  {
+    tittle: "Нейминг",
+    link: "/",
+  },
+  {
+    tittle: "Логотип",
+    link: "/",
+  },
+  {
+    tittle: "Полиграфия",
+    link: "/",
+  },
+];
+
 export default function Footer() {
   return (
     <div className="flex justify-between w-full h-40 items-center p-7 bg-secondary">
       <div className="flex self-start text-7xl">Перейдем на “мы”? ;)</div>
       <div className="flex text-xs gap-12 self-start opacity-40">
-        <div className="flex flex-col gap-1.5">
-          <Link href={"/"}>
-            <div>Обо мне</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Проекты</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Услуги</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Блог</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Контакты</div>
-          </Link>
-        </div>
-        <div className="flex flex-col gap-1.5">
-          <Link href={"/"}>
-            <div>Фирменный стиль</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Нейминг</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Логотип</div>
-          </Link>
-          <Link href={"/"}>
-            <div>Полиграфия</div>
-          </Link>
+        <div className=" grid grid-cols-2 gap-1.5">
+          {NavigationLink.map((tag) => {
+            return (
+              <Link href={tag.link} key={tag.tittle}>
+                <div>{tag.tittle}</div>
+              </Link>
+            );
+          })}
         </div>
       </div>
       <div className="flex gap-1.5 self-start">
