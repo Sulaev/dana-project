7c57ddb6eeb80cc5d6754908d03ad9691f5ee51b
Sulaev <sulaev2004@mail.ru>
2025-05-06 14:58:25 +0300

    save edit

---
+++ src/app/home/blog/page.tsx
@@ -0,0 +1,5 @@
+import Blog from "@/components/pages/blog";
+
+export default function Page() {
+  return <Blog />;
+}
--- src/components/blocks/blogSection.tsx
+++ src/components/blocks/blogSection.tsx
@@ -3,26 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 import BigArrow from "@/assets/bigArrow.svg";
 
-export default function BlogSection() {
-  const BlogList = [
-    {
-      title: "Почему дешевый дизайн обходится дороже?",
-      description:
-        "Популярные кейсы, где отчаянные попытки сэкономить привели к потере клиентов или репутации.",
-      publishDate: "20.01.2020",
-      baner: BlogPlaceHolder,
-      link: "/",
-    },
-    {
-      title: "Почему дешевый дизайн обходится дешевле?",
-      description:
-        "Популярные кейсы, где отчаянные попытки сэкономить привели к потере клиентов или репутации.",
-      publishDate: "20.01.2020",
-      baner: BlogPlaceHolder,
-      link: "/",
-    },
-  ];
+const BlogList = [
+  {
+    title: "Почему дешевый дизайн обходится дороже?",
+    description:
+      "Популярные кейсы, где отчаянные попытки сэкономить привели к потере клиентов или репутации.",
+    publishDate: "20.01.2020",
+    baner: BlogPlaceHolder,
+    link: "/",
+    category: "about-project",
+  },
+  {
+    title: "Почему дешевый дизайн обходится дешевле?",
+    description:
+      "Популярные кейсы, где отчаянные попытки сэкономить привели к потере клиентов или репутации.",
+    publishDate: "20.01.2020",
+    baner: BlogPlaceHolder,
+    link: "/",
+    category: "Wiki-disign",
+  },
+];
 
+export default function BlogSection() {
   return (
     <div className="flex flex-col w-full py-28 gap-16">
       <div>
