640df76e3fcc34fcff47c92e05e762169ae3d32d
Sulaev <sulaev2004@mail.ru>
2025-05-06 13:05:10 +0300

    new code design services

--- src/components/pages/services.tsx
+++ src/components/pages/services.tsx
@@ -1,10 +1,15 @@
-import ServicesTittle from "../blocks/services/services";
 import ServicesSection from "../blocks/servicesSection";
 
 export default function Services() {
   return (
     <>
-      <ServicesTittle />
+      <div className="flex flex-col w-full gap-14 py-10">
+        <p className="opacity-60">Главная / Услуги</p>
+        <h1 className="text-4xl font-medium max-w-[824px]">
+          Мы создаём единое смысловое поле вокруг бренда. Не угадываем, а
+          проектируем опыт взаимодействия с компанией и знаем как
+        </h1>
+      </div>
       <ServicesSection />
     </>
   );
